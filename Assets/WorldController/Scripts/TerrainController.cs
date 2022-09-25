6670682741271d3dffa845003ba831b39c4b9e15
unknown <gabrielsilveirapro@gmail.com>
2022-09-25 16:52:13 -0300

    Fix threaded world generation

--- Assets/WorldController/Scripts/TerrainController.cs
+++ Assets/WorldController/Scripts/TerrainController.cs
@@ -21,49 +21,58 @@ public class TerrainController : MonoBehaviour
     
     private Texture2D texture;
     public Dictionary<Vector2, float[,]> instantiatedChuncks = new Dictionary<Vector2, float[,]>();
+
+    private Thread t1;
+    private GameObject[] players;
  
     void Start ()
     {
-        
+        t1 = new Thread(HandleTerrainChuncks) {Name = "Thread 1"};
+        t1.Start();
     }
 
-    void Update()
-    {
-        HandleTerrainChuncks();
-    }
 
+    void FixedUpdate() {
+        players = GameObject.FindGameObjectsWithTag("Player");
+    }
+    
     public void HandleTerrainChuncks() 
     {
         
-        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        while(true) {
+            if(players == null)
+                continue;
 
-        foreach (GameObject player in players)
-        {
-            Vector2 rootChunck = new Vector2((int)(player.transform.position.x / size), (int)(player.transform.position.z / size));
-
-            for(int x = (int)rootChunck.x - 2; x <= (int)rootChunck.x + 2; x++)
+            foreach (GameObject player in players)
             {
-                for(int z = (int)rootChunck.y - 2; z <= (int)rootChunck.y + 2; z++)
+                Vector2 rootChunck = new Vector2();
+
+                UnityMainThread.wkr.AddJob(() => {
+                    rootChunck = new Vector2((int)(player.transform.position.x / size), (int)(player.transform.position.z / size));
+                });  
+                Thread.Sleep(200);  
+                
+                for(int x = (int)rootChunck.x - 2; x <= (int)rootChunck.x + 2; x++)
                 {
-                    Vector2 areaChunck = new Vector2(rootChunck.x + x, rootChunck.y + z);
-                    
-                    if(!instantiatedChuncks.ContainsKey(areaChunck))
+                    for(int z = (int)rootChunck.y - 2; z <= (int)rootChunck.y + 2; z++)
                     {
-                        instantiatedChuncks.Add(areaChunck, null);
-                        UnityMainThread.wkr.AddJob(() => {
+                        Vector2 areaChunck = new Vector2(rootChunck.x + x, rootChunck.y + z);
+                        
+                        if(!instantiatedChuncks.ContainsKey(areaChunck))
+                        {
+                            instantiatedChuncks.Add(areaChunck, null);
                             noiseMap = Noise.GenerateNoiseMap(size, size, scale, octaves, redistribuition, areaChunck);
                             instantiatedChuncks[areaChunck] = noiseMap;
-                        }); 
-
-                        UnityMainThread.wkr.AddJob(() => {
                             noiseMap = FallOffGenerator.ApplyFallOffMap(noiseMap, size);
-                        }); 
 
-                        UnityMainThread.wkr.AddJob(() => {
-                            GameObject terrain = TerrainGenerator.GenerateTerrain(noiseMap, terrainMaterial, areaChunck);
-                            GameObject oceanTerrainChunck = GameObject.Instantiate(oceanChunck, terrain.transform.position, terrain.transform.rotation, terrain.transform);
-                            oceanTerrainChunck.transform.position = new Vector3(oceanTerrainChunck.transform.position.x + 125f, 4f, oceanTerrainChunck.transform.position.z + 125f);
-                        });                              
+                            UnityMainThread.wkr.AddJob(() => {
+                                GameObject terrain = TerrainGenerator.GenerateTerrain(noiseMap, terrainMaterial, areaChunck);
+                                GameObject oceanTerrainChunck = GameObject.Instantiate(oceanChunck, terrain.transform.position, terrain.transform.rotation, terrain.transform);
+                                oceanTerrainChunck.transform.position = new Vector3(oceanTerrainChunck.transform.position.x + 125f, 4f, oceanTerrainChunck.transform.position.z + 125f);
+                            });      
+
+                            Thread.Sleep(300);                        
+                        }
                     }
                 }
             }
